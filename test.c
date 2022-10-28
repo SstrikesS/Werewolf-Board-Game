3aab9dcdad11dc1df7ef40f8e04ad936d58723e6
Nguyen Tien Thanh <thanhnguyentb2001@gmail.com>
2022-10-28 22:49:19 +0700

    fix

--- test.c
+++ test.c
@@ -1,4 +1,5 @@
 #include <SDL2/SDL.h>
+<<<<<<< Updated upstream
 #include <SDL2/SDL_rect.h>
 #include <SDL2/SDL_render.h>
 #include <SDL2/SDL_ttf.h>
@@ -72,5 +73,24 @@ int main(int argc, char** argv) {
             } 
         }
     }
+=======
+#include <SDL2/SDL_image.h>
+#include <SDL2/SDL_timer.h>
+ 
+int main(int argc, char *argv[])
+{
+ 
+    // returns zero on success else non-zero
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        printf("error initializing SDL: %s\n", SDL_GetError());
+    }
+    SDL_Window* win = SDL_CreateWindow("GAME",
+                                       SDL_WINDOWPOS_CENTERED,
+                                       SDL_WINDOWPOS_CENTERED,
+                                       1000, 1000, 0);
+    while (1)
+        ;
+ 
+>>>>>>> Stashed changes
     return 0;
 }
